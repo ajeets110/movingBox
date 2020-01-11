b01f9b6783de8bb8c6b5a9ad0b60d76af9d0b9ca
Ajeet Singh <ajeets110@gmail.com>
2020-01-10 21:35:06 -0500

    init

--- prac/ViewController.swift
+++ prac/ViewController.swift
@@ -10,11 +10,66 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var box: UILabel!
+  
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        moveDown()
+    
+        let swipeRight = UISwipeGestureRecognizer(target: self, action: #selector(swipedRight))
+        swipeRight.direction = UISwipeGestureRecognizer.Direction.right
+        view.addGestureRecognizer(swipeRight)
     }
-
-
+    
+    @objc func swipedRight(gestureRecognizer : UIGestureRecognizer)  {
+        print("swiping right eh?")
+    }
+    
+    func moveDown() {
+       
+        UIView.animate(withDuration: 3.0, delay: 0, options: .transitionCurlDown, animations: {
+            
+            self.box.frame.origin.y = self.box.frame.origin.y + 818
+            
+        }) { (fin) in
+            if fin {
+               
+                print("fin")
+                self.moveUp()
+                
+            }
+        }
+    }
+    func moveRight(){
+        print("moving right")
+//        let guide = self.view.safeAreaLayoutGuide
+//        let width = guide.layoutFrame.width
+            UIView.animate(withDuration: 3.0, delay: 0, options: .transitionCurlDown, animations: {
+                
+                
+                self.box.frame.origin.x = self.view.frame.width + self.view.safeAreaInsets.right
+            }) { (fin) in
+                if fin {
+                    self.box.frame.origin.x = self.view.frame.width + self.view.safeAreaInsets.right
+                    self.moveUp()
+                }
+            }
+        }
+    func moveUp() {
+        print("moving up")
+        UIView.animate(withDuration: 3.0, delay: 0, options: .transitionCurlDown, animations: {
+            
+            self.box.frame.origin.y = self.box.frame.origin.y - 818
+            
+        }) { (fin) in
+            if fin {
+                print("fin")
+                self.moveDown()
+                
+            }
+        }
+    }
 }
+
 
